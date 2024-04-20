e8f7fe0bea566e03e79aa2d5aedffd02f9ea1a0a
Furkan Metin <metin.furkan016@gmail.com>
2024-04-20 17:57:14 +0300

    views created

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import ToDoItem
 
 # Create your views here.
+
+def todo_list(request):
+    todos=ToDoItem.objects.all()
+    return render(request,'todo/todo_list.html',{'todos':todos})
+
